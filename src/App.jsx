399a48a760a0d1bc620d32ea61b0c2cafe7f0945
JohnWooSpendsy <john.woo@spendsy.com>
2025-04-14 17:57:01 +0800

    responsive design

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,9 @@
-// App.js
 import React from 'react';
 import './App.css';
 
 // Simple SVG icons
 const WindIcon = () => (
-  <svg viewBox="0 0 24 24" width="24" height="24" stroke="#777" strokeWidth="2" fill="none">
+  <svg viewBox="0 0 24 24" width="18" height="18" stroke="#777" strokeWidth="2" fill="none">
     <path d="M17.7 7.7a2.5 2.5 0 1 1 1.8 4.3H2" />
     <path d="M9.6 4.6A2 2 0 1 1 11 8H2" />
     <path d="M12.6 19.4A2 2 0 1 0 14 16H2" />
@@ -12,7 +11,7 @@ const WindIcon = () => (
 );
 
 const HumidityIcon = () => (
-  <svg viewBox="0 0 24 24" width="24" height="24" stroke="#777" strokeWidth="2" fill="none">
+  <svg viewBox="0 0 24 24" width="18" height="18" stroke="#777" strokeWidth="2" fill="none">
     <path d="M12 2.69l5.66 5.66a8 8 0 1 1-11.31 0L12 2.69z" />
   </svg>
 );
@@ -130,17 +129,6 @@ function App() {
                 <div className="hour-time">6 PM</div>
                 <div className="hour-temp">21°</div>
                 <div className="hour-condition">Cloudy</div>
-              </div>
-              {/* Extra hourly items for ultrawide displays */}
-              <div className="hour-item ultrawide-only">
-                <div className="hour-time">7 PM</div>
-                <div className="hour-temp">20°</div>
-                <div className="hour-condition">Cloudy</div>
-              </div>
-              <div className="hour-item ultrawide-only">
-                <div className="hour-time">8 PM</div>
-                <div className="hour-temp">19°</div>
-                <div className="hour-condition">Partly Cloudy</div>
               </div>
             </div>
           </div>
